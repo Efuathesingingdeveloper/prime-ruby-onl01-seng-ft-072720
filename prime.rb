358be0fdf6ee1418b7579c2a71711d1a9d1769b1
Georgette Class-Peters <georgycp@udel.edu>
2020-07-28 17:19:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,6 @@
    return false if !number.integer?
    return false if < 2
    return true if number == 2
-   (2..number-1).each 
+   (2..number-1).each {|int| return false if number % int == 0}
   end 
  end 
