ea9bd8c82bdf8605ef9a0178b02067cb6a3b1acd
Georgette Class-Peters <georgycp@udel.edu>
2020-07-28 17:17:33 +0000

    Done.

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -1,21 +1,9 @@
 require_relative './spec_helper'
 require_relative '../prime.rb'
 
-describe "prime?" do
-  it 'returns true for prime numbers' do
-    expect(prime?(2)).to be(true)
-    expect(prime?(3)).to be(true)
-    expect(prime?(11)).to be(true)
-    expect(prime?(105557)).to be(true)
-  end
-
-  it 'returns false for non-prime numbers' do
-    expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
-    expect(prime?(0)).to be(false)
-    expect(prime?(1)).to be(false)
-    expect(prime?(4)).to be(false)
-    expect(prime?(40)).to be(false)
-    expect(prime?(1763)).to be(false)
-    expect(prime?(101013)).to be(false)
-  end
+def is_prime?(number)
+return false if !number.integer? 
+return false if number < 2
+return true if number == 2
+(2..number-1).each
 end
